475927acf91dc0117e9fc86ed09186d40f94c072
kd542 <kd542@cam.ac.uk>
2025-01-29 09:23:33 +0000

    Added motor file

---
+++ MOTORworking.py
@@ -0,0 +1,45 @@
+from machine import Pin, PWM 
+class Motor: 
+    def __init__(self): 
+        self.m1Dir = Pin(4 , Pin.OUT)         # set pin left wheel 
+        self.pwm1 = PWM(Pin(5))           
+        self.pwm1.freq(1000) 
+        self.pwm1.duty_u16(0)
+        
+        self.m2Dir = Pin(7 , Pin.OUT)         # set pin left wheel 
+        self.pwm2 = PWM(Pin(6))           
+        self.pwm2.freq(1000) 
+        self.pwm2.duty_u16(0)
+        
+    def off(self): 
+        self.pwm1.duty_u16(0)
+        self.pwm2.duty_u16(0)
+        
+    def Reverse(self): 
+        self.m1Dir.value(0)                     # forward = 0 reverse = 1 motor 1 
+        self.pwm1.duty_u16(int(65535*100/100))
+        self.m2Dir.value(0)                     # forward = 0 reverse = 1 motor 1 
+        self.pwm2.duty_u16(int(65535*100/100))
+        # speed range 0-100 motor 1 
+    def Forward(self): 
+        self.m1Dir.value(1) 
+        self.pwm1.duty_u16(int(65535*100/100))
+        self.m2Dir.value(1) 
+        self.pwm2.duty_u16(int(65535*100/100))
+        
+    def left_turn(self, speed = 100): 
+        self.m1Dir.value(0) 
+        self.pwm1.duty_u16(int(65535*speed/100))
+        self.m2Dir.value(1) 
+        self.pwm2.duty_u16(int(65535*speed/100))
+        
+    def right_turn(self, speed = 100): 
+        self.m1Dir.value(1) 
+        self.pwm1.duty_u16(int(65535*speed/100))
+        self.m2Dir.value(0) 
+        self.pwm2.duty_u16(int(65535*speed/100))
+        
+        
+        
+        
+        
