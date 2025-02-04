5bc4ee17b27d98206409a0f5dc0aabbb896fe01b
MYLO175 <mylomichael@icloud.com>
2025-02-04 13:04:16 +0000

    Updated MOTOR and created navigation2

---
+++ Navigation2.py
@@ -0,0 +1,16 @@
+from MOTOR import Motor
+
+from LFollow import LineFollowing
+
+path = ["f", "l", "f"]
+
+def navigation(path):
+    for step in path:
+        detected = False
+        while detected = False:
+            LFollow.Follow_line()
+            
+            
+            
+        
+    
