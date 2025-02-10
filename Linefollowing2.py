bacfe434c6f132522ebee163004deec429c02201
MYLO175 <mylomichael@icloud.com>
2025-02-10 13:16:07 +0000

    10/02/25

--- Linefollowing2.py
+++ Linefollowing2.py
@@ -14,16 +14,6 @@ class LineFollowing:
         self.junction2 = Pin(26, Pin.IN)
         
         
-        
-
-                     
-
-
-    def adjust(self, turn): #takes in how long we have been on white line, calculates a reduced speed to send to one of the motors
-        while self.left_sensor.value()!=1 or self.right_sensor.value() !=1:
-            Motor.adjust_direction(turn)
-            
-
             
         
     def Follow_line(self):
@@ -38,14 +28,78 @@ class LineFollowing:
             right_value = self.right_sensor.value()
             
             if left_value == 1 and right_value == 1: #Both sensors detect line
-               
                 Motor.Forward()
                 
-            elif left_value == 1 and right_value == 0: #therefore off to the right of the line
-                self.adjust( turn="left")
+            elif left_value == 1 and right_value == 0: #therefore off to the right of the line                
+                Motor.adjust_direction("left")
+                while (self.left_sensor.value()!=1 or self.right_sensor.value() !=1) and ((self.junction1.value() and self.junction2.value()) ==0):
+                    sleep(0.01)
                 
             elif left_value == 0 and right_value == 1:
-                self.adjust( turn="right")
+                Motor.adjust_direction("right")
+                while (self.left_sensor.value()!=1 or self.right_sensor.value() !=1) and ((self.junction1.value() and self.junction2.value()) ==0):
+                    sleep(0.01)
+                    
+            sleep(0.01)
+                        
+        Motor.off()                                   
+                
+        sleep(0.01)
+        #break
+        
+         
+         
+    def turn(self, direction, angle):#
+        if direction == "cw":
+            Motor.cw_spin(50)
+            sleep(0.005 * angle)
+                
+        elif direction == "acw":
+            Motor.acw_spin(50)
+            sleep(0.005 * angle)
+        
+        
+        done = False
+        while done ==False:
+            left_value = self.left_sensor.value()
+            right_value = self.right_sensor.value()
+            if left_value == 1 or right_value == 1: #Both sensors detect line
+                done = True
+                sleep(0.2)
+            
+                
+        
+        Motor.Forward()
+        sleep(0.5)
+        Motor.off()
+        #break
+        
+        
+        
+        
+        
+        
+        
+    def Rev_Follow_line(self):
+        
+        junction1 = self.junction1.value()
+        junction2 = self.junction2.value()
+        
+        while (junction1 or junction2) == 0 :
+            junction1 = self.junction1.value()
+            junction2 = self.junction2.value()
+            left_value = self.left_sensor.value()
+            right_value = self.right_sensor.value()
+            
+            if left_value == 1 and right_value == 1: #Both sensors detect line
+               
+                Motor.Reverse()
+                
+            elif left_value == 1 and right_value == 0: #therefore off to the right of the line
+                self.rev_adjust( turn="right")
+                
+            elif left_value == 0 and right_value == 1:
+                self.rev_adjust( turn="left")
                     
             sleep(0.01)
                         
@@ -53,33 +107,3 @@ class LineFollowing:
                             
                 
         sleep(0.01)
-        #break
-        
-            
-    def turn(self, direction, angle):#
-        done = False
-        while done ==False:
-            
-            if direction == "cw":
-                Motor.cw_spin(50)
-                sleep(0.005 * angle)
-                
-            elif direction == "acw":
-                Motor.acw_spin(50)
-                sleep(0.005 * angle)
-                
-            done2 = False
-            while done2 == False:
-                left_value = self.left_sensor.value()
-                right_value = self.right_sensor.value()
-                if left_value == 1 or right_value == 1: #Both sensors detect line
-                    done2 = True
-                sleep(0.2)
-                
-            done = True
-                
-        
-        Motor.Forward()
-        sleep(0.5)
-        Motor.off()
-        #break
--- MOTOR2.py
+++ MOTOR2.py
@@ -1,8 +1,9 @@
 from machine import Pin, PWM
 
 
-
-speed = 100
+speed=100
+r2 = 1
+r1 = 0.9
 
 class Motor: 
     def __init__(self): 
@@ -21,39 +22,52 @@ class Motor:
         self.pwm2.duty_u16(0)
         
     def Reverse(self): 
-        self.m1Dir.value(0)                     # forward = 0 reverse = 1 motor 1 
-        self.pwm1.duty_u16(int(65535*100/100))
-        self.m2Dir.value(0)                     # forward = 0 reverse = 1 motor 1 
-        self.pwm2.duty_u16(int(65535*100/100))
+        self.m1Dir.value(1)                     # forward = 0 reverse = 1 motor 1 
+        self.pwm1.duty_u16(int(65535*40*r1/100))
+        self.m2Dir.value(1)                     # forward = 0 reverse = 1 motor 1 
+        self.pwm2.duty_u16(int(65535*40*r2/100))
         # speed range 0-100 motor 1
         
     def Forward(self): 
         self.m1Dir.value(0) 
-        self.pwm1.duty_u16(int(65535*speed/100))
+        self.pwm1.duty_u16(int(65535*speed*r1/100))
         self.m2Dir.value(0) 			
-        self.pwm2.duty_u16(int(65535*speed/100))
+        self.pwm2.duty_u16(int(65535*speed*r2/100))
         
     def acw_spin(self, speed): 
         self.m1Dir.value(0) 
-        self.pwm1.duty_u16(int(65535*speed/100))
+        self.pwm1.duty_u16(int(65535*speed*r1/100))
         self.m2Dir.value(1) 
-        self.pwm2.duty_u16(int(65535*speed/100))
+        self.pwm2.duty_u16(int(65535*speed*r2/100))
         
     def cw_spin(self, speed): 
         self.m1Dir.value(1) 
-        self.pwm1.duty_u16(int(65535*speed/100))
+        self.pwm1.duty_u16(int(65535*speed*r1/100))
         self.m2Dir.value(0) 
-        self.pwm2.duty_u16(int(65535*speed/100))
+        self.pwm2.duty_u16(int(65535*speed*r2/100))
         
     
     def adjust_direction(self, turn):
         if turn == "left":
             self.m1Dir.value(0) 
-            self.pwm1.duty_u16(int(65535*speed/100))
+            self.pwm1.duty_u16(int(65535*speed*r1/100))
             self.m2Dir.value(0) 			
-            self.pwm2.duty_u16(int(65535*(speed*0.8)/100))
+            self.pwm2.duty_u16(int(65535*(speed*r2*0.85)/100))
         elif turn == "right":
             self.m1Dir.value(0)
-            self.pwm1.duty_u16(int(65535*(speed*0.8)/100))
+            self.pwm1.duty_u16(int(65535*(speed*r1*0.85)/100))
             self.m2Dir.value(0)
-            self.pwm2.duty_u16(int(65535*speed/100))
+            self.pwm2.duty_u16(int(65535*speed*r2/100))
+            
+    
+    def rev_adjust_direction(self, turn):
+        if turn == "right":
+            self.m1Dir.value(1) 
+            self.pwm1.duty_u16(int(65535*(r1*40*1)/100))
+            self.m2Dir.value(1) 			
+            self.pwm2.duty_u16(int(65535*(r2*40*0.9)/100))
+        elif turn == "left":
+            self.m1Dir.value(1)
+            self.pwm1.duty_u16(int(65535*(r1*40*0.9)/100))
+            self.m2Dir.value(1)
+            self.pwm2.duty_u16(int(65535*(r2*40*1)/100))
