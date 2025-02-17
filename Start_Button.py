424882152c0b50d23b744d6eda5b5603f4834f15
kd542 <kd542@cam.ac.uk>
2025-02-17 17:13:39 +0000

    Button not really working...

--- Start_Button.py
+++ Start_Button.py
@@ -10,8 +10,12 @@ def check_button():
     global button_pressed  # Access global variable
 
     if button.value() == 1:
-        led.value(1)  # Turn LED on
+        #led.value(1)  # Turn LED on
         sleep(0.5)  # Debounce delay
         button_pressed = True  # Latch the button state
 
     return 1 if button_pressed else 0  # Keep value at 1 after press
+
+"""while True:
+    print(button.value())
+    sleep(1)"""
